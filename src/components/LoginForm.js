71d30139d09f07fd5676bf4eaebb6862c92c9c5b
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-06-13 20:14:38 -0300

    oculte el formulario del login y agregue dos botones para mostrarlo y para ocultarlo

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,27 +1,43 @@
+import { useState } from "react"
+
 export default function LoginForm(props) {
+
+  const [loginVisible, setLoginVisible] = useState(false)
+
+  const hideWhenVisible = { display: loginVisible ? 'none': ''}
+  const showWhenVisible = { display: loginVisible ? '': 'none'}
+
   return (
-    <form onSubmit={props.handlerSubmit}>
-      <div>
-        <input
-          type='text'
-          value={props.username}
-          name='Username'
-          placeholder='Username'
-          onChange={props.handlerUsernameChange}
-        />
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={() => setLoginVisible(true)}>Login</button>
       </div>
-      <div>
-        <input
-          type='password'
-          value={props.password}
-          name='Password'
-          placeholder='Password'
-          onChange={props.handlerPasswordChange}
-        />
+      <div style={showWhenVisible}>
+        <form onSubmit={props.handlerSubmit}>
+          <div>
+            <input
+              type='text'
+              value={props.username}
+              name='Username'
+              placeholder='Username'
+              onChange={props.handlerUsernameChange}
+            />
+          </div>
+          <div>
+            <input
+              type='password'
+              value={props.password}
+              name='Password'
+              placeholder='Password'
+              onChange={props.handlerPasswordChange}
+            />
+          </div>
+          <button>
+            Login
+          </button>
+        </form>
+        <button onClick={()=> setLoginVisible(false)}>Cancel</button>
       </div>
-      <button>
-        Login
-      </button>
-    </form>
+    </div>
   )
 }
